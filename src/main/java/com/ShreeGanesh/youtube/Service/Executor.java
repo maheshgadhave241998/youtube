bc403f8a807be48da9299c3c994fb8d7b162a4e5
mahesh <maheshgadhave241998@gmail.com>
2026-05-23 20:49:55 +0530

    railway check

--- src/main/java/com/ShreeGanesh/youtube/Service/Executor.java
+++ src/main/java/com/ShreeGanesh/youtube/Service/Executor.java
@@ -168,7 +168,8 @@ public class Executor {
 
             ProcessBuilder builder = new ProcessBuilder(
                     ytDlp+
-                    " --cookies " +cookiesPath+
+//                    " --cookies " +cookiesPath+
+                            " --cookies-from-browser --user-agent "+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"+
                     " -F "+
                     url
             );
