26c768284065913412b47055f5398fe66d689c5c
mahesh <maheshgadhave241998@gmail.com>
2026-05-24 21:02:04 +0530

    railway check

--- src/main/java/com/ShreeGanesh/youtube/config/CorsConfig.java
+++ src/main/java/com/ShreeGanesh/youtube/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://youtube-cbc6.up.railway.app")
+                        .allowedOrigins("https://y2vids.up.railway.app")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
